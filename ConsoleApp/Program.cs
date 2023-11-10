abeaf1d58c73ea48ac236cec75e8e9806daeeb91
Said <said@example.com>
2023-11-10 22:19:06 +0300

    Create GrokAlgProj and BinarySearch class

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using SearchAlg;
+
+namespace ConsoleApp;
+
+public class Program
+{
+    public static void Main()
+    {
+        BinarySearchTest();
+    }
+
+    private static void BinarySearchTest()
+    {
+        var testArr = new[] {1, 3, 5, 7, 9};
+        Console.WriteLine(BinarySearch<int>.BinarySearchFunc(1, testArr));
+        Console.WriteLine(BinarySearch<int>.BinarySearchFunc(4, testArr));
+    }
+}
---
+++ SearchAlg/BinarySearch.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SearchAlg;
+
+public static class BinarySearch<T>
+where T : IComparable<T> 
+{
+    public static int? BinarySearchFunc(T item, T[] sortedArray)
+    {
+        var low = 0;
+        var high = sortedArray.Length - 1;
+        while (low <= high)
+        {
+            var mid = (low + high) / 2;
+            var guess = sortedArray[mid];
+            if (item.Equals(guess))
+                return mid;
+            if (guess.CompareTo(item) > 0)
+                low = mid + 1;
+            else
+                high = mid - 1;
+        }
+        return null;
+    }
+}

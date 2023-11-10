806580ddfd7ddff5b3c5cac564e565f34f4f9040
Said <said@example.com>
2023-11-10 22:30:28 +0300

    Add xml comments to binarysearch

--- SearchAlg/BinarySearch.cs
+++ SearchAlg/BinarySearch.cs
@@ -3,12 +3,27 @@ using System.Collections.Generic;
 using System.Linq;
 
 namespace SearchAlg;
-
+/// <summary>
+/// Класс, реализующий бинарный поиск.
+/// </summary>
 public static class BinarySearch<T>
 where T : IComparable<T> 
 {
+    /// <summary>
+    /// Бинарным (или двоичным) называют поиск элемента упорядоченного множества через многократное
+    /// деление этого множества пополам. Искомый элемент всегда будет оказываться в одной
+    /// из двух частей.
+    /// Поиск прекращается, когда обнаруживается совпадение граничного элемента
+    /// между двумя разделенными блоками с заданным,
+    /// или когда заданный элемент не обнаруживается вовсе.
+    ///Реализация этого метода возможна только применимо к отсортированным множествам.
+    /// </summary>
+    ///<param name="item">Искомый элемент.</param>
+    ///<param name="sortedArray">Отсортированный массив элементов.</param>
+    /// <returns>Индекс искомого элемента или null при его отсутствии.</returns>
     public static int? BinarySearchFunc(T item, T[] sortedArray)
     {
+        ///
         var low = 0;
         var high = sortedArray.Length - 1;
         while (low <= high)

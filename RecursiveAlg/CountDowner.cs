b9bbbb41e4d92c8868e01385a3efdfe4c139aa6d
Said <said@example.com>
2023-11-13 18:44:10 +0300

    add CountDownerRec

---
+++ RecursiveAlg/CountDowner.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RecursiveAlg
+{
+    public class CountDowner
+    {
+        public static void CountDownerRec(int num)
+        {
+            if (num < 0)
+                return;
+            Console.WriteLine(num);
+            CountDownerRec(num--);
+        }
+    }
+}
